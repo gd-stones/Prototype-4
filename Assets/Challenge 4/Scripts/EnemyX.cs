2dd6d09826413928d1bb567e90c0cf99cfd64590
Nguyen Duc Viet <ngducviet2105@gmail.com>
2023-04-22 21:14:29 +0700

    complete bonus challenge 4

--- Assets/Challenge 4/Scripts/EnemyX.cs
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -8,8 +8,10 @@ public class EnemyX : MonoBehaviour
     public float speed = 1;
     private Rigidbody enemyRb;
     public GameObject playerGoal;
-    int markEnemy = 0;
-    int markPlayer = 0;
+    int waveCount = SpawnManagerX.waveCount;
+
+    public int markEnemy = 0;
+    public int markPlayer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,23 +25,23 @@ public class EnemyX : MonoBehaviour
     {
         // Set enemy direction towards player goal and move there
         Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed);
+        enemyRb.AddForce(lookDirection * speed * waveCount);
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other) // can doc lai logic cua ham nay
     {
         // If enemy collides with either goal, destroy it
         if (other.gameObject.name == "Enemy Goal")
         {
             Destroy(gameObject);
-            markEnemy++;
-            Debug.Log("Mark Enemy: " + markEnemy.ToString());
+            markPlayer++;
+            Debug.Log("Mark Player: " + markPlayer.ToString());
         }
         else if (other.gameObject.name == "Player Goal")
         {
             Destroy(gameObject);
-            markPlayer++;
-            Debug.Log("Mark Player: " + markPlayer.ToString());
+            markEnemy++;
+            Debug.Log("Mark Enemy: " + markEnemy.ToString()); // tai sao luon in ra 1
         }
     }
 }
--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -11,9 +11,12 @@ public class PlayerControllerX : MonoBehaviour
     public bool hasPowerup;
     public GameObject powerupIndicator;
     public int powerUpDuration = 5;
-
+    
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private float turboBoostSpeed = 1;
+    public ParticleSystem turboDustParticle;
 
     void Start()
     {
@@ -25,10 +28,12 @@ public class PlayerControllerX : MonoBehaviour
     {
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * turboBoostSpeed * Time.deltaTime);
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        TurboBoost();
     }
 
     // If Player collides with powerup, activate powerup
@@ -67,6 +72,21 @@ public class PlayerControllerX : MonoBehaviour
             {
                 enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
             }
+        }
+    }
+
+    void TurboBoost()
+    {
+        // turboBoostSpeed changed with spacebar key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            turboBoostSpeed = 2;
+            turboDustParticle.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            turboBoostSpeed = 1;
+            turboDustParticle.Stop();
         }
     }
 }
